e1a066938941c9cc3b2a6524beed59ef467811ce
Remco Bloemen <remco@wicked.ventures>
2025-02-23 17:10:55 +0100

    zkhash

--- merkle-hash-bench/src/hashes/poseidon2_t3_zkhash.rs
+++ merkle-hash-bench/src/hashes/poseidon2_t3_zkhash.rs
@@ -1,10 +1,10 @@
+#![cfg(feature = "zkhash")]
 use {
-    crate::{register_hash, Field, HashFn, SmolHasher},
-    zkhash::{
-        ark_ff::{BigInteger, PrimeField, Zero},
-        fields::bn256::FpBN256,
+    crate::{register_hash, Field, HashFn, SmolHasher}, bytemuck::cast, zkhash::{
+        ff::{Field as _, PrimeField},
+        fields::bn256::{FpBN256, FrRepr},
         poseidon2::{poseidon2::Poseidon2, poseidon2_instance_bn256::POSEIDON2_BN256_PARAMS},
-    },
+    }
 };
 
 register_hash!(Poseidon2T3Zkhash::new());
@@ -25,17 +25,23 @@ impl SmolHasher for Poseidon2T3Zkhash {
     }
 
     fn hash(&self, messages: &[u8], hashes: &mut [u8]) {
+        fn to_field(mut val: [u8;32]) -> FpBN256 {
+            val[31] = 0; // NOTE: Do not do in prod.
+            FpBN256::from_raw_repr(FrRepr(cast::<[u8;32],[u64;4]>(val))).unwrap()
+        }
+
+
         for (message, hash) in messages.chunks_exact(64).zip(hashes.chunks_exact_mut(32)) {
             let mut state = [
-                FpBN256::from_le_bytes_mod_order(&message[0..32]),
-                FpBN256::from_le_bytes_mod_order(&message[32..64]),
+                to_field(message[0..32].try_into().unwrap()),
+                to_field(message[32..64].try_into().unwrap()),
                 FpBN256::zero(),
             ];
             self.0.permutation(&mut state);
 
             // This allocates a Vec, which is dumb but it's the only way to get the bytes
             // our of arkworks.
-            hash.copy_from_slice(state[0].0.to_bytes_le().as_ref());
+            hash.copy_from_slice(&cast::<[u64;4],[u8;32]>(state[0].into_raw_repr().0));
         }
     }
 }
