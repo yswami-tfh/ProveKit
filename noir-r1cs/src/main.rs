5a28570e633585729af3df446d917cb0751321e8
Remco Bloemen <remco@wicked.ventures>
2025-04-03 13:40:42 +0200

    Measuring allocator and span statistics

--- noir-r1cs/src/main.rs
+++ noir-r1cs/src/main.rs
@@ -5,8 +5,12 @@ use {
     self::cmd::Command,
     anyhow::Result,
     std::{
+        alloc::{GlobalAlloc, Layout, System as SystemAlloc},
         fmt::{Display, Formatter},
-        sync::{Arc, Mutex},
+        sync::{
+            atomic::{AtomicUsize, Ordering},
+            Arc, Mutex,
+        },
         time::Instant,
     },
     tracing::{span, Subscriber},
@@ -17,6 +21,29 @@ use {
         Layer, Registry,
     },
 };
+
+#[global_allocator]
+static ALLOC: MeasuringAllocator = MeasuringAllocator::new();
+
+/// Custom allocator that keeps track of statistics to see program memory
+/// consumption.
+struct MeasuringAllocator {
+    current: AtomicUsize,
+    max:     AtomicUsize,
+    count:   AtomicUsize,
+}
+
+/// Logging layer that keeps track of time and memory consumption.
+struct DurationLayer {
+    start: Arc<Mutex<Vec<SpanStart>>>,
+}
+
+struct SpanStart {
+    time:        Instant,
+    memory:      usize,
+    max:         usize,
+    allocations: usize,
+}
 
 fn main() -> Result<()> {
     let fmt_layer = fmt::Layer::default();
@@ -30,8 +57,46 @@ fn main() -> Result<()> {
     args.run()
 }
 
-struct DurationLayer {
-    start: Arc<Mutex<Vec<Instant>>>,
+impl MeasuringAllocator {
+    const fn new() -> Self {
+        Self {
+            current: AtomicUsize::new(0),
+            max:     AtomicUsize::new(0),
+            count:   AtomicUsize::new(0),
+        }
+    }
+
+    fn current(&self) -> usize {
+        self.current.load(Ordering::SeqCst)
+    }
+
+    fn max(&self) -> usize {
+        self.max.load(Ordering::SeqCst)
+    }
+
+    fn reset_max(&self) {
+        self.max.store(self.current(), Ordering::SeqCst);
+    }
+
+    fn count(&self) -> usize {
+        self.count.load(Ordering::SeqCst)
+    }
+}
+
+#[allow(unsafe_code)]
+unsafe impl GlobalAlloc for MeasuringAllocator {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        // We just ignore the race conditions here...
+        let prev = self.current.fetch_add(layout.size(), Ordering::SeqCst);
+        self.max.fetch_max(prev + layout.size(), Ordering::SeqCst);
+        self.count.fetch_add(1, Ordering::SeqCst);
+        SystemAlloc.alloc(layout)
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        self.current.fetch_sub(layout.size(), Ordering::SeqCst);
+        SystemAlloc.dealloc(ptr, layout)
+    }
 }
 
 impl DurationLayer {
@@ -49,7 +114,21 @@ where
     fn on_enter(&self, id: &span::Id, ctx: Context<'_, S>) {
         let mut lock = self.start.lock().unwrap();
         let depth = lock.len();
-        lock.push(Instant::now());
+
+        // Propagate current max down the stack.
+        let max = ALLOC.max();
+        for entry in lock.iter_mut() {
+            entry.max = std::cmp::max(entry.max, max);
+        }
+        ALLOC.reset_max();
+
+        lock.push(SpanStart {
+            time:        Instant::now(),
+            memory:      ALLOC.current(),
+            max:         ALLOC.current(),
+            allocations: ALLOC.count(),
+        });
+
         let span = ctx.span(id).expect("expected: span id exists in registry");
 
         for _ in 0..depth {
@@ -62,7 +141,12 @@ where
     fn on_exit(&self, id: &span::Id, ctx: Context<'_, S>) {
         let mut lock = self.start.lock().unwrap();
         let depth = lock.len();
-        let duration = lock.pop().expect("expected: start time exists").elapsed();
+        let start = lock.pop().expect("expected: start time exists");
+
+        let duration = start.time.elapsed();
+        let max = std::cmp::max(ALLOC.max(), start.max);
+        let allocations = ALLOC.count() - start.allocations;
+        let own = max - start.memory;
 
         let _span: tracing_subscriber::registry::SpanRef<'_, S> =
             ctx.span(id).expect("expected: span id exists in registry");
@@ -71,7 +155,13 @@ where
             eprint!("│ ");
         }
 
-        eprintln!("├─╯ {}s", human(duration.as_secs_f64()));
+        eprintln!(
+            "├─╯ took {}s, {} allocations, {}B peak memory, {}B own memory",
+            human(duration.as_secs_f64()),
+            human(allocations as f64),
+            human(max as f64),
+            human(own as f64)
+        );
     }
 }
 
