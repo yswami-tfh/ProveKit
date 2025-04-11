848218247d038cff69def1f9a1e44b1d763ca872
Ben Wilson <benjamin@asmusas.net>
2025-04-11 13:17:35 +1000

    Brilling test

--- noir-r1cs/src/test_compiler.rs
+++ noir-r1cs/src/test_compiler.rs
@@ -2,18 +2,20 @@ use std::fs::File;
 use noirc_artifacts::program::ProgramArtifact;
 use crate::{compiler::R1CS, solver::MockTranscript, utils::file_io::deserialize_witness_stack};
 
-// Tests that a simple ACIR program containing a read-only memory block can be compile to R1CS and
-// the witness solved for.
-#[test]
-fn test_read_only_memory() {
-    let file = File::open("src/test_programs/read-only-memory/target/main.json").unwrap();
+// Tests that an ACIR program containing a can be compiled to R1CS and
+// the R1CS witness solved for given the ACIR witness.
+fn test_compilation_and_solving(
+    program_path: &str,
+    witness_path: &str,
+) {
+    let file = File::open(program_path).unwrap();
     let program: ProgramArtifact = serde_json::from_reader(file).unwrap();
     let acir_circuit = &program.bytecode.functions[0];
     // Compile the ACIR circuit to R1CS
     let r1cs = R1CS::from_acir(acir_circuit);
 
     // Solve for the R1CS witness using the ACIR witness
-    let mut witness_stack = deserialize_witness_stack("src/test_programs/read-only-memory/target/main.gz").unwrap();
+    let mut witness_stack = deserialize_witness_stack(witness_path).unwrap();
     let acir_witness = witness_stack.pop().unwrap().witness;
 
     let mut transcript = MockTranscript::new();
@@ -21,4 +23,20 @@ fn test_read_only_memory() {
 
     // Check that the witness satisfies the R1CS relation
     assert!(r1cs.matrices.test_satisfaction(&witness).is_none());
+}
+
+#[test]
+fn test_read_only_memory() {
+    test_compilation_and_solving(
+        "src/test_programs/read-only-memory/target/main.json",
+        "src/test_programs/read-only-memory/target/main.gz",
+    );
+}
+
+#[test]
+fn test_brillig_conditional() {
+    test_compilation_and_solving(
+        "src/test_programs/brillig-conditional/target/main.json",
+        "src/test_programs/brillig-conditional/target/main.gz",
+    );
 }
