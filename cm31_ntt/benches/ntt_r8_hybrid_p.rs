c27b45a1f41b148e4209faaa3462a19189899956
Koh Wei Jie <contact@kohweijie.com>
2025-04-30 13:39:21 -0700

    tweaked benches/ntt_r8_hybrid_p.rs; deleted unused old files

--- cm31_ntt/benches/ntt_r8_hybrid_p.rs
+++ cm31_ntt/benches/ntt_r8_hybrid_p.rs
@@ -9,36 +9,54 @@ use rand_chacha::rand_core::SeedableRng;
 use lazy_static::lazy_static;
 
 lazy_static! {
-    static ref PRECOMP: PrecomputedTwiddles = {
+    static ref PRECOMP_7: PrecomputedTwiddles = {
+        let n = 8usize.pow(7);
+        precompute_twiddles(n).unwrap()
+    };
+
+    static ref PRECOMP_8: PrecomputedTwiddles = {
         let n = 8usize.pow(8);
         precompute_twiddles(n).unwrap()
     };
 }
 
 fn bench(c: &mut Criterion) {
+    let mut rng = ChaCha8Rng::seed_from_u64(0);
+
     let mut group = c.benchmark_group("ntt_r8_hybrid_p");
 
-    for log8_n in 7..9 {
-        let n = 8usize.pow(log8_n);
-        //let wn = get_root_of_unity(n);
-        //let precomp_small = precomp_for_ntt_r8_ip_p(NTT_BLOCK_SIZE_FOR_CACHE, get_root_of_unity(NTT_BLOCK_SIZE_FOR_CACHE));
-        //let precomp_full = gen_precomp_full(n, wn, NTT_BLOCK_SIZE_FOR_CACHE);
+    // 8 ^ 7
+    let n = 8usize.pow(7);
 
-        let mut rng = ChaCha8Rng::seed_from_u64(0);
-
-        let mut f = vec![CF::zero(); n];
-        for i in 0..n {
-            f[i] = rng.r#gen();
-        }
-
-        let mut scratch = vec![CF::zero(); NTT_BLOCK_SIZE_FOR_CACHE];
-
-        group.bench_function(format!("size {n}"), |b| {
-            b.iter(|| {
-                ntt_r8_hybrid_p(black_box(&f), &mut scratch, &*PRECOMP);
-            })
-        });
+    let mut f = vec![CF::zero(); n];
+    for i in 0..n {
+        f[i] = rng.r#gen();
     }
+
+    let mut scratch = vec![CF::zero(); NTT_BLOCK_SIZE_FOR_CACHE];
+
+    group.bench_function(format!("size {n}"), |b| {
+        b.iter(|| {
+            ntt_r8_hybrid_p(black_box(&f), &mut scratch, &*PRECOMP_7);
+        })
+    });
+
+    // 8 ^ 8
+
+    let n = 8usize.pow(8);
+
+    let mut f = vec![CF::zero(); n];
+    for i in 0..n {
+        f[i] = rng.r#gen();
+    }
+
+    let mut scratch = vec![CF::zero(); NTT_BLOCK_SIZE_FOR_CACHE];
+
+    group.bench_function(format!("size {n}"), |b| {
+        b.iter(|| {
+            ntt_r8_hybrid_p(black_box(&f), &mut scratch, &*PRECOMP_8);
+        })
+    });
 
     group.finish();
 }
