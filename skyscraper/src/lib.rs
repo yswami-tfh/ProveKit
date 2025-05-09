e4709a4087620a9bc00b5931dfaea03d05fa4b4d
Remco Bloemen <remco@wicked.ventures>
2025-05-09 15:04:31 +0200

    Note on autotune

--- skyscraper/src/lib.rs
+++ skyscraper/src/lib.rs
@@ -3,6 +3,7 @@
 #![feature(cold_path)]
 
 pub mod arithmetic;
+pub mod autotune;
 pub mod bar;
 pub mod block;
 pub mod block3;
@@ -12,3 +13,9 @@ pub mod reduce;
 pub mod reference;
 pub mod simple;
 pub mod v1;
+
+pub type CompressManyFn = fn(&[u8], &mut [u8]);
+
+// TODO: Some autotune method that does a small benchmark on target hardware and
+// finds the optimal implementation. It should also have an enum that can be
+// stored in a file. And the benchmarking should be instrumented.
