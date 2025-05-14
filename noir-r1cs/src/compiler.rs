8b729a1bbe0c8b8375cde0717aac6dbcee94cfef
Ben Wilson <benjamin@asmusas.net>
2025-05-14 10:19:25 +1000

    Docfixes

--- noir-r1cs/src/compiler.rs
+++ noir-r1cs/src/compiler.rs
@@ -435,7 +435,7 @@ impl R1CS {
                             *rt_witness,
                         ));
                         range_check.push(*rt_witness);
-                        range_check.push(difference_witness_idx);
+                        //FIXME range_check.push(difference_witness_idx);
                     });
             }
         });
@@ -852,10 +852,16 @@ pub enum MemoryOperation {
 // FIXME where does this belong?
 #[derive(Debug, Clone)]
 pub(crate) struct SpiceWitnesses {
+    /// The length of the memory block
     pub memory_length: usize,
+    /// The witness index of the first initial value (they are stored contiguously)
+    /// (Not written to)
     pub initial_values_start: usize,
+    /// The memory operations, in the order that they occur; each SpiceMemoryOperation contains witness indices that will be written to)
     pub memory_operations: Vec<SpiceMemoryOperation>,
+    /// The witness index of the first of the memory_length final read values (stored contiguously) (these witnesses are written to)
     pub rv_final_start: usize,
+    /// The witness index of the first of the memory_length final read timestamps (stored contiguously) (these witnesses are written to)
     pub rt_final_start: usize,
     /// The index of the first witness written to by the SpiceWitnesses struct
     pub first_witness_idx: usize,
@@ -907,12 +913,16 @@ impl SpiceWitnesses {
     }
 }
 
-/// Like MemoryOperation, but with the indices of the additional witnesses needed by Spice.
+/// Like [MemoryOperation], but with the indices of the additional witnesses needed by Spice.
 #[derive(Debug, Clone)]
 pub(crate) enum SpiceMemoryOperation {
-    /// (R1CS witness index of address, R1CS witness index of value read)
+    /// Load operation.  Arguments are R1CS witness indices:
+    /// (address, value read, read timestamp)
+    /// `address` is already solved for by the ACIR solver.
     Load(usize, usize, usize),
-    /// (R1CS witness index of address, R1CS index of old value, R1CS witness index of value to write, R1CS witness index of the read timestamp)
+    /// Store operation.  Arguments are R1CS witness indices:
+    /// (address, old value, new value, read timestamp)
+    /// `address`, `old value`, `new value` are already solved for by the ACIR solver.
     Store(usize, usize, usize, usize),
 }
 
