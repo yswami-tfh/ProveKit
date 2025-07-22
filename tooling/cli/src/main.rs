2ea90d77a0bc21b3a131cc1929ba4eb9684dcb6e
Piotr Heilman <piotr.heilman@toolsforhumanity.com>
2025-07-22 14:24:32 +0200

    Add tracy tool for profiling.

--- tooling/cli/src/main.rs
+++ tooling/cli/src/main.rs
@@ -1,22 +1,55 @@
 #![allow(missing_docs)]
 mod cmd;
+#[cfg(feature = "cli-profiled")]
 mod measuring_alloc;
+#[cfg(feature = "cli-profiled")]
 mod span_stats;
 
+use std::clone::Clone;
+use std::convert::Into;
+use self::cmd::Command;
+use anyhow::Result;
+
+#[cfg(feature = "cli-profiled")]
 use {
-    self::{cmd::Command, measuring_alloc::MeasuringAllocator, span_stats::SpanStats},
-    anyhow::Result,
+    tracing::subscriber,
+    tracing_subscriber::Layer,
+    crate::measuring_alloc::{MeasuringAllocatorState, MeasuringAllocator},
+    span_stats::SpanStats,
     tracing_subscriber::{self, layer::SubscriberExt as _, Registry},
 };
 
+#[cfg(feature = "cli-profiled")]
+static ALLOC_STATE: MeasuringAllocatorState = MeasuringAllocatorState::new();
+
+#[cfg(feature = "cli-profiled")]
 #[global_allocator]
-static ALLOC: MeasuringAllocator = MeasuringAllocator::new();
+static ALLOC_TRACY: tracy_client::ProfiledAllocator<MeasuringAllocator> = tracy_client::ProfiledAllocator::new(MeasuringAllocator::new(&ALLOC_STATE), 100);
 
+#[cfg(not(feature = "cli-profiled"))]
 fn main() -> Result<()> {
-    let subscriber = Registry::default().with(SpanStats);
-    tracing::subscriber::set_global_default(subscriber)?;
-
     // Run CLI command
     let args = argh::from_env::<cmd::Args>();
     args.run()
+}
+
+#[cfg(feature = "cli-profiled")]
+fn main() -> Result<()> {
+    let subscriber = Registry::default()
+        .with(tracing_tracy::TracyLayer::default())
+        .with(SpanStats);
+
+    subscriber::set_global_default(subscriber)?;
+
+    let _client = tracy_client::Client::start();
+
+    // Run CLI command
+    let args = argh::from_env::<cmd::Args>();
+    let res = args.run();
+
+    unsafe {
+        tracy_client_sys::___tracy_shutdown_profiler();
+    }
+
+    res
 }
--- tooling/cli/src/measuring_alloc.rs
+++ tooling/cli/src/measuring_alloc.rs
@@ -6,12 +6,16 @@ use std::{
 /// Custom allocator that keeps track of statistics to see program memory
 /// consumption.
 pub struct MeasuringAllocator {
+    state: &'static MeasuringAllocatorState
+}
+
+pub struct MeasuringAllocatorState {
     current: AtomicUsize,
     max:     AtomicUsize,
     count:   AtomicUsize,
 }
 
-impl MeasuringAllocator {
+impl MeasuringAllocatorState {
     pub const fn new() -> Self {
         Self {
             current: AtomicUsize::new(0),
@@ -39,18 +43,44 @@ impl MeasuringAllocator {
     }
 }
 
+impl MeasuringAllocator {
+    pub const fn new(state: &'static MeasuringAllocatorState) -> Self {
+        Self {
+            state
+        }
+    }
+
+    pub fn current(&self) -> usize {
+        self.state.current.load(Ordering::SeqCst)
+    }
+
+    pub fn max(&self) -> usize {
+        self.state.max.load(Ordering::SeqCst)
+    }
+
+    pub fn reset_max(&self) -> usize {
+        let current = self.current();
+        self.state.max.store(current, Ordering::SeqCst);
+        current
+    }
+
+    pub fn count(&self) -> usize {
+        self.state.count.load(Ordering::SeqCst)
+    }
+}
+
 #[allow(unsafe_code)]
 unsafe impl GlobalAlloc for MeasuringAllocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
         // We just ignore the race conditions here...
-        let prev = self.current.fetch_add(layout.size(), Ordering::SeqCst);
-        self.max.fetch_max(prev + layout.size(), Ordering::SeqCst);
-        self.count.fetch_add(1, Ordering::SeqCst);
+        let prev = self.state.current.fetch_add(layout.size(), Ordering::SeqCst);
+        self.state.max.fetch_max(prev + layout.size(), Ordering::SeqCst);
+        self.state.count.fetch_add(1, Ordering::SeqCst);
         SystemAlloc.alloc(layout)
     }
 
     unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
-        self.current.fetch_sub(layout.size(), Ordering::SeqCst);
+        self.state.current.fetch_sub(layout.size(), Ordering::SeqCst);
         SystemAlloc.dealloc(ptr, layout);
     }
 }
